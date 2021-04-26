a7837fb7910c5c4cac26eaccc2e63c308b582079
DerToaster98 <seiler.oli@web.de>
2021-04-26 15:01:43 +0200

    ammo item & registry

---
+++ src/de/dertoaster/dtarmory/ammunition/DTArmoryAmmoRegistry.java
@@ -0,0 +1,12 @@
+package de.dertoaster.dtarmory.ammunition;
+
+import de.dertoaster.dtlib.spigot.items.CustomItemRegistry;
+import de.dertoaster.dtlib.spigot.items.IPluginProvider;
+
+public class DTArmoryAmmoRegistry extends CustomItemRegistry<ItemAmmunition> {
+
+	public DTArmoryAmmoRegistry(IPluginProvider plugin) {
+		super(plugin);
+	}
+
+}
---
+++ src/de/dertoaster/dtarmory/ammunition/ItemAmmunition.java
@@ -0,0 +1,138 @@
+package de.dertoaster.dtarmory.ammunition;
+
+import java.util.Optional;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.data.Openable;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import de.dertoaster.dtarmory.DTArmory;
+import de.dertoaster.dtarmory.data.IDTArmoryAmmoData;
+import de.dertoaster.dtlib.spigot.items.CustomItemBase;
+import de.dertoaster.dtlib.spigot.items.ItemDamageUtil;
+import de.dertoaster.dtlib.spigot.items.ItemDataUtil;
+
+public class ItemAmmunition extends CustomItemBase {
+	
+	protected final IDTArmoryAmmoData ammoData;
+	
+	public static final NamespacedKey SHOT_COUNT_DATA_KEY = new NamespacedKey(DTArmory.getInstance(), "dtarmory_ammo_shot_key");
+
+	public ItemAmmunition(final IDTArmoryAmmoData ammoData) {
+		super(ammoData.getItemIdent(), DTArmory.AMMO_REGISTRY);
+		
+		this.ammoData = ammoData;
+	}
+
+	@Override
+	public boolean canItemBeUsed(Player player, ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	protected ItemStack getDefault() {
+		ItemStack stack = new ItemStack(this.ammoData.getMinecraftItemID());
+		ItemMeta meta = stack.getItemMeta();
+		
+		meta.setUnbreakable(this.ammoData.isItemUnbreakable());
+		meta.setCustomModelData(this.ammoData.getCustomModelData());
+		meta.setDisplayName(this.ammoData.getDisplayName());
+		meta.setLore(this.ammoData.getItemDescription());
+		
+		stack.setItemMeta(meta);
+		
+		return stack;
+	}
+
+	@Override
+	public void onRightClick(ItemStack item, PlayerInteractEvent event) {
+		if(event.getClickedBlock().getState() instanceof Openable) {
+		} else {
+			event.setCancelled(true);
+		}
+	}
+
+	@Override
+	public void onLeftClick(ItemStack item, PlayerInteractEvent event) {
+
+	}
+
+	@Override
+	public void onInteractAtEntity(ItemStack item, Entity clicked, PlayerInteractAtEntityEvent event) {
+
+	}
+
+	@Override
+	public void onDrop(ItemStack item, PlayerDropItemEvent event) {
+
+	}
+
+	@Override
+	public void onInventoryClick(ItemStack item, InventoryClickEvent event) {
+
+	}
+
+	@Override
+	public void onInventoryDrag(ItemStack item, InventoryDragEvent event) {
+
+	}
+
+	@Override
+	public void onDispense(ItemStack item, BlockDispenseEvent event) {
+
+	}
+
+	@Override
+	public void onEquip(ItemStack item, PlayerItemHeldEvent event) {
+
+	}
+
+	@Override
+	public void onUnequip(ItemStack item, PlayerItemHeldEvent event) {
+
+	}
+
+	@Override
+	public void onSwapTo(EquipmentSlot newHand, boolean isDualWield, ItemStack item, PlayerSwapHandItemsEvent event) {
+
+	}
+	
+	public static int getShotsInAmmo(ItemStack item) {
+		Optional<Integer> itemData = ItemDataUtil.tryGetPersistentInteger(SHOT_COUNT_DATA_KEY, item);
+		if(itemData.isPresent()) {
+			return itemData.get();
+		}
+		return -1;
+	}
+	
+	public ItemStack[] generateItemStackWithShotCount(int shotCount) {
+		int stackCount = (int) Math.ceil(shotCount / this.ammoData.getShotCount());
+		ItemStack[] result = new ItemStack[stackCount];
+		int shotsToLoad = shotCount;
+		for(int i = 0; i < stackCount; i++) {
+			int shots = shotsToLoad - this.ammoData.getShotCount() > 0 ? this.ammoData.getShotCount() : shotsToLoad;
+			shotsToLoad -= this.ammoData.getShotCount();
+			if(shots <= 0) {
+				result[i] = null;
+				break;
+			}
+			result[i] = this.getDefault();
+			ItemDataUtil.trySetPersistentInteger(SHOT_COUNT_DATA_KEY, shots, result[i]);
+			ItemDamageUtil.setDamagePercent(shots / this.ammoData.getShotCount(), result[i]);
+		}
+		return result;
+	}
+
+}
