2ee2a213c98707807d4a006d9a2e3cc2637b0407
DerToaster98 <seiler.oli@web.de>
2021-05-04 18:56:16 +0200

    add sounds

--- src/de/dertoaster/dtarmory/data/IDTArmoryGunData.java
+++ src/de/dertoaster/dtarmory/data/IDTArmoryGunData.java
@@ -1,10 +1,19 @@
 package de.dertoaster.dtarmory.data;
 
+import de.dertoaster.dtlib.spigot.sound.DTLibComplexSound;
+
 public interface IDTArmoryGunData extends IDTArmoryItemDataBase {
 	
 	public int getAmmoCapacity();
 	
+	//Timers
 	public long getShotDelay();
 	public long getReloadDuration();
+	
+	//Sounds
+	public DTLibComplexSound getReloadingSound();
+	public DTLibComplexSound getOutOfAmmoSound();
+
+	public String getRequiredAmmo();
 	
 }
